9708508decb43a2f9c1cc097b4272b98a6234eea
thallysfs <thallys@hotmail.com>
2021-11-15 10:37:37 -0300

    primeiro commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+    colors: {
+        primary: '#5636d3',
+        
+        secondary: '##ff872c',
+        secondary_ligth: 'rgba(255, 135, 44, 0.3)',
+
+        success: '#12a454',
+        succes_ligth: 'rgba(18,164,84, 0.5)',
+        
+        attention: '#e83f5b',
+        attention_ligth: 'rgba(232, 63, 91, 0.5)'
+
+    }
+
+}
