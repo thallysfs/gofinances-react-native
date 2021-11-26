597e7c2f92327e27384dff1ca3beb19a9e95c22f
thallysfs <thallys@hotmail.com>
2021-11-26 15:49:55 -0300

    criação de tela register, category select

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button } from '../../Components/Form/Button'
 
 import { Input } from '../../Components/Form/Input'
+import { TransactionTypeButton } from '../../Components/Form/TransactionTypeButton'
+import { CategorySelect } from '../../Components/Form/CategorySelect'
 
 import { 
     Container,
     Header,
     Title,
-    Form 
+    Form,
+    Fields,
+    TransactionsTypes 
 } from './styles'
 
 export function Register(){
+  const [transactionType, setTransactionType] = useState('');
+
+  function handleTransactionTypeSelect(type: 'up' | 'down' ){
+    setTransactionType(type);
+  }
+
   return(
     <Container>
       <Header>
@@ -18,12 +28,31 @@ export function Register(){
       </Header>
 
       <Form>
-        <Input 
-          placeholder="Nome"
-        />
-        <Input 
-          placeholder="Preço"
-        />
+        <Fields>
+          <Input 
+            placeholder="Nome"
+          />
+          <Input 
+            placeholder="Preço"
+          />
+
+          <TransactionsTypes>
+            <TransactionTypeButton 
+              type='up'
+              title='Income'
+              onPress={() => handleTransactionTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />          
+            <TransactionTypeButton 
+              type='down'
+              title='Outcome'
+              onPress={ () => handleTransactionTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionsTypes>
+
+          <CategorySelect title="Categoria" />
+        </Fields>
 
         <Button title="Enviar" />
       </Form>
--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -25,7 +25,20 @@ export const Title = styled.Text`
 
 export const Form = styled.View`
     flex: 1;
+    justify-content: space-between;
     width: 100%;
     padding: 24px;
+`;
+
+export const Fields = styled.View`
+    
+`;
+
+export const TransactionsTypes = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+
+    margin-top: 8px;
+    margin-bottom: 16px;
 `;
 
