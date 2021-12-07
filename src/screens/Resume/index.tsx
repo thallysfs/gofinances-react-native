83eae914a829c40c04bb93a76a485455a7e3d28c
thallysfs <thallys@hotmail.com>
2021-12-07 15:22:03 -0300

    criando month select

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { VictoryPie } from 'victory-native'
+import { RFValue } from 'react-native-responsive-fontsize';
 
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import { useTheme } from 'styled-components'
 
 import { HistoryCard } from '../../Components/HistoryCard'
@@ -12,9 +14,12 @@ import {
     Header,
     Title,
     Content,
-    ChartContainer 
+    ChartContainer,
+    MonthSelect,
+    MonthSelectButton,
+    MonthSelectIcon,
+    Month 
 } from './styles'
-import { RFValue } from 'react-native-responsive-fontsize';
 
 interface TransactionData {
     type: 'positive' | 'negative';
@@ -101,7 +106,27 @@ export function Resume(){
             <Title>Resumo por categoria</Title>
         </Header>
 
-        <Content>
+        <Content
+           showsVerticalScrollIndicator={false}
+           contentContainerStyle={{
+            paddingHorizontal: 24,
+            paddingBottom: useBottomTabBarHeight()
+           }} 
+        >
+
+            <MonthSelect>
+                <MonthSelectButton>
+                    <MonthSelectIcon name="chevron-left" />
+                </MonthSelectButton>
+
+                <Month>Maio</Month>
+
+                <MonthSelectButton>
+                    <MonthSelectIcon name="chevron-right" />
+                </MonthSelectButton>
+            </MonthSelect>
+
+
             <ChartContainer>
                 <VictoryPie 
                 data={totalByCategories}
